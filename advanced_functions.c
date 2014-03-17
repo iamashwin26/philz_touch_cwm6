228dfd7a3d144ae1163af448985b5a819fdb97a3
philz-cwm6 <phytowardt@gmail.com>
2014-03-17 23:53:29 +0100

    fix compiling for Advanced Edition

--- advanced_functions.c
+++ advanced_functions.c
@@ -3667,7 +3667,9 @@ void show_philz_settings_menu()
                 ui_print(EXPAND(RECOVERY_MOD_VERSION) "\n");
                 ui_print("Build version: " EXPAND(PHILZ_BUILD) "-Blackhawk-f2fs - " EXPAND(TARGET_COMMON_NAME) "\n");
                 ui_print("CWM Base version: " EXPAND(CWM_BASE_VERSION) "\n");
+#ifdef PHILZ_TOUCH_RECOVERY
                 print_libtouch_version(1);
+#endif
                 //ui_print(EXPAND(BUILD_DATE)"\n");
                 ui_print("Dual-Boot-Solution by ketut.kumajaya @ XDA" "\n");
                 ui_print("Compiled by Android-Andi @ XDA - %s at %s\n", __DATE__, __TIME__);
